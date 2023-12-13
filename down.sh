7f27e8ed715add85f937fb0cd08294ffd92ceaeb
jonathan <celluloid-demon@users.noreply.github.com>
2023-12-12 22:08:20 -0800

    lol

--- down.sh
+++ down.sh
@@ -9,6 +9,17 @@ SETTINGS="./settings"
 # Initialize script
 init() {
 
+	# Create settings file if it doesn't already exist
+	if [ ! -e "$SETTINGS" ]; then
+
+		echo "\'$SETTINGS\' file not found, creating..."
+
+		cp ${RESOURCE_DIR}/$(basename "$SETTINGS")* ./
+
+		mv "$SETTINGS"* "$SETTINGS"
+		
+	fi
+
 	# Source settings file
 	. "$SETTINGS"
 
